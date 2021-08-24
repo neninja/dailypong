4d36111f9ead5b98f162dcb604c3cc22f63a7e9b
Felipe Silva <felipe.dacs@gmail.com>
2021-08-24 12:52:37 -0300

    :new: add mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [ colega, setColega ] = useState("")
+  const [ colegas, setColegas ] = useState<string[]>([])
+
+  useEffect(() => {
+    let c = localStorage.getItem('@dailypong') ?? '[]'
+    setColegas(JSON.parse(c))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('@dailypong', JSON.stringify(colegas))
+  }, [colegas])
+
+  function handleSubmit(e: React.FormEvent){
+    e.preventDefault()
+
+    setColegas([...colegas, colega])
+    setColega("")
+  }
+
+  function handleExcluir(index: number){
+    setColegas(colegas.filter((c, i) => i !== index))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={colega} onChange={e => setColega(e.target.value)}/>
+        <input type="submit" value="Cadastrar colega"/>
+      </form>
+
+      <table>
+        <tr>
+          <th>Falou</th>
+          <th>Colega</th>
+          <th>Excluir</th>
+        </tr>
+        {colegas.map((nome, i) => (
+          <tr key={i}>
+            <td>
+              <input type='checkbox'/>
+            </td>
+            <td>
+              {nome}
+            </td>
+            <td>
+              <button onClick={e => handleExcluir(i)}>Excluir</button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </main>
   );
 }
 
